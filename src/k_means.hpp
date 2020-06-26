f40951b98e7af325895f607889c0ec0991fe6c5c
John <indianajohn@github>
2020-06-25 23:08:32 -0700

    Clean up of design

--- src/k_means.hpp
+++ src/k_means.hpp
@@ -3,18 +3,30 @@
 
 namespace basic_matrix {
 namespace k_means {
+
+/// A single cluster.
 struct KMeansCluster {
+  /// Centroid of the cluster.
   Matrix centroid;
+  /// Indices in the original poitn set of points in the cluster.
   std::vector<size_t> member_indices;
 };
+
+/// The result of a k-means clustering.
 struct KMeansClustering {
-  basic_matrix::Matrix centroid;
+  /// Clusters
   std::vector<KMeansCluster> clusters;
 };
 
 struct KMeansOptions {
+  /// Maximum number of iterations.
   size_t max_iterations = 100;
+
+  /// Number of cluster centers.
   size_t k = 10;
+
+  // Movement center for all thresholds
+  double movement_threshold = 0.0;
 };
 
 /// Clusters a vector of N-dimensional points (each point much be the
