409c777c0e7dd363db9ad0afb7713b46b6cd8a7a
John <indianajohn@github>
2020-07-02 13:09:16 -0700

    More tests

--- tests/io.cpp
+++ tests/io.cpp
@@ -1,4 +1,5 @@
 #include "io.hpp"
+#include "matrix_helpers.hpp"
 #include "test_helpers.hpp"
 
 using namespace basic_matrix;
@@ -8,25 +9,40 @@ inline std::string generateTestDataWithoutNewline() {
   return "0.3,2e3,3.2e3\n999,.4,-3\n1.5e-3,2,1e-3";
 }
 inline std::string generateTestDataWithNewline() {
-  return "0.3,2e3,3.2e3\n999,.4,-3\n1.5e-3,2,1e-3";
+  return "0.3,2e3,3.2e3\n999,.4,-3\n1.5e-3,2,1e-3\n";
 }
 }; // namespace
 
 void testStringLoading() {
-  Matrix expected = {{0.3, 2e3, 3.2e3}, {999, 0.4, -3}, {1.5e-3, 2, 1e-3}};
   {
+    Matrix expected = {{0.3, 2e3, 3.2e3}, {999, 0.4, -3}, {1.5e-3, 2, 1e-3}};
     Matrix matrix = parseFromString(generateTestDataWithoutNewline());
-    ASSERT_EQ(matrix.width(), expected.width());
-    ASSERT_EQ(matrix.height(), expected.height());
-    double error = (matrix - expected).norm();
-    ASSERT_NEAR(error, 0);
+    assertMatrixNear(matrix, expected);
   }
   {
+    Matrix expected = {{0.3, 2e3, 3.2e3}, {999, 0.4, -3}, {1.5e-3, 2, 1e-3}};
     Matrix matrix = parseFromString(generateTestDataWithNewline());
-    ASSERT_EQ(matrix.width(), expected.width());
-    ASSERT_EQ(matrix.height(), expected.height());
-    double error = (matrix - expected).norm();
-    ASSERT_NEAR(error, 0);
+    assertMatrixNear(matrix, expected);
+  }
+  {
+    Matrix matrix = parseFromString("");
+    Matrix expected = {};
+    assertMatrixNear(matrix, expected);
+  }
+  {
+    Matrix matrix = parseFromString("3");
+    Matrix expected = {3};
+    assertMatrixNear(matrix, expected);
+  }
+  {
+    Matrix matrix = parseFromString("3,4.0");
+    Matrix expected = {3, 4.0};
+    assertMatrixNear(matrix, expected);
+  }
+  {
+    Matrix matrix = parseFromString("3\n4.0");
+    std::vector<std::vector<double>> expected = {{3.}, {4.0}};
+    assertMatrixNear(matrix, Matrix(expected));
   }
 }
 
